4032f712ef78f2609ca357dc1cbcec04f1e3b769
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-15 16:29:34 +0200

    moved hello sample code to separate file, now just injection of App into DOM

--- src/index.jsx
+++ src/index.jsx
@@ -2,41 +2,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "../assets/stylesheets/application.scss";
-
-// const Hello = (props) => {
-//   return <div>Hello, {props.name}</div>;
-// };
-
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: false,
-    };
-  }
-
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked,
-    });
-  };
-
-  render() {
-    return (
-      <div
-        className={this.state.clicked ? "clicked" : null}
-        onClick={this.handleClick}
-      >
-        Hello {this.props.name}
-      </div>
-    );
-  }
-}
+import App from "./components/app.jsx";
 
 const root = document.getElementById("root");
 if (root) {
-  ReactDOM.render(<Hello name="Alex" />, root);
+  ReactDOM.render(<App />, root);
 }
-
-console.log(Component);
